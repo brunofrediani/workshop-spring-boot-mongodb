b2e69a44d47eb1ca1ae7f3d6b585f96fdca759ad
Bruno Frediani <brunojfredi@gmail.com>
2022-12-11 01:24:57 -0300

    Obtendo um usuário por id

--- src/main/java/com/example/workshopmongo/domain/User.java
+++ src/main/java/com/example/workshopmongo/domain/User.java
@@ -12,8 +12,7 @@ public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	private String id;
-	
+	private String id;	
 	private String name;
 	private String email;
 	
